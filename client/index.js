158f3ab691d3400f1137056d4487df62ed81d6f3
Syanmil <syanmil@yahoo.com>
2017-02-12 17:19:31 +0700

    complete and delete done

--- client/index.js
+++ client/index.js
@@ -1,18 +1,20 @@
-$(document).ready(function(){
-
-})
-
 $('#newForm').on('submit', function(e) {
   e.preventDefault()
   let content = $('#newForm input.form-control').val()
   $('#TodoList').append(
-    `<tr>
+    `<tr id="tr${$('tr').length}">
       <td>${content}</td>
-      <td>Uncomplete</td>
+      <td id="td${$('tr').length}">Uncomplete</td>
       <td>
-        <button type="button" class="btn btn-info delete" name="complete">Complete</button>
-        <button type="button" class="btn btn-danger delete" name="delete">Delete</button>
+        <button type="button" class="btn btn-info delete" name="complete" onclick="complete(${$('tr').length})">Complete</button>
+        <button type="button" class="btn btn-danger delete" name="delete" onclick="destroy(${$('tr').length})">Delete</button>
       </td>
     </tr>`
   )
 })
+function destroy(id){
+  $(`#tr${id}`).empty()
+}
+function complete(id){
+  $(`#td${id}`).text("Complete")
+}
